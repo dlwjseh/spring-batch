8891f37038b60450066c0fc06a5e37a81be93f95
LeeJeondo <jeondo.lee@kbfg.com>
2022-07-19 11:13:27 +0900

    simpleJob step1 성공시킴 - execution 데이터 확인

--- src/main/java/kr/co/kbds/alfredbatch/job/SimpleJobConfiguration.java
+++ src/main/java/kr/co/kbds/alfredbatch/job/SimpleJobConfiguration.java
@@ -33,7 +33,9 @@ public class SimpleJobConfiguration {
 	public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
 		return stepBuilderFactory.get("simpleStep1") // simpleStep1 이란 이름의 Batch Step을 생성. 마찬가지로 Builder를 통해 이름을 지정
 				.tasklet((contribution, chunkContext) -> { // Step 안에서 수행될 기능들을 명시. Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
-					throw new IllegalArgumentException("step1에서 실패합니다.");
+					log.info(">>>>> This is Step1");
+					log.info(">>>>> requestDate = {}", requestDate);
+					return RepeatStatus.FINISHED;
 				})
 				.build();
 	}
