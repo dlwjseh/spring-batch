f890462d32c555443ccf41c4ea0fd21ba4b19ea8
LeeJeondo <jeondo.lee@kbfg.com>
2022-07-19 10:36:35 +0900

    simpleJob requestDate 파라미터 추가

--- src/main/java/kr/co/kbds/alfredbatch/job/SimpleJobConfiguration.java
+++ src/main/java/kr/co/kbds/alfredbatch/job/SimpleJobConfiguration.java
@@ -3,8 +3,10 @@ package kr.co.kbds.alfredbatch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,15 +23,17 @@ public class SimpleJobConfiguration {
 	@Bean
 	public Job simpleJob() {
 		return jobBuilderFactory.get("simpleJob") // simpleJob 이란 이름의 Batch Job을 생성. job의 이름은 별도로 지정하지 않고, 이렇게 Builder를 통해 지정
-				.start(simpleStep1())
+				.start(simpleStep1(null))
 				.build();
 	}
 
 	@Bean
-	public Step simpleStep1() {
+	@JobScope
+	public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
 		return stepBuilderFactory.get("simpleStep1") // simpleStep1 이란 이름의 Batch Step을 생성. 마찬가지로 Builder를 통해 이름을 지정
 				.tasklet((contribution, chunkContext) -> { // Step 안에서 수행될 기능들을 명시. Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
 					log.info(">>>>> This is Step1");
+					log.info(">>>>> requestDate = {}", requestDate);
 					return RepeatStatus.FINISHED;
 				})
 				.build();
